3eb57fab64abcc71ddc197f13cea365850dbb449
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2024-01-26 22:33:37 +0200

    adds a few more plugins

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "RRethy/nvim-treesitter-endwise"
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+
+      treesitter.setup({
+        auto_install = true,
+        highlight = {
+          enable = true,
+        },
+        endwise = {
+          enable = true,
+        },
+      })
+    end,
+  }
+}
