c3b8e7b5376215962e97dbf3a8f01913ebcd2dd5
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2024-02-14 16:22:33 +0200

    adds new telescope keymapping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,13 +21,17 @@ return {
       local tel_grep_string = function()
         tel_builtin.grep_string(tel_themes.get_dropdown())
       end
+      local tel_lsp_document_symbols = function()
+        tel_builtin.lsp_document_symbols(tel_themes.get_dropdown())
+      end
 
       local opts = { noremap = true, silent = true }
       vim.keymap.set("n", "<leader>ff", tel_find_files, opts)
       vim.keymap.set("n", "<leader>fg", tel_live_grep, opts)
       vim.keymap.set("n", "<leader>fb", tel_buffers, opts)
       vim.keymap.set("n", "<leader>fr", tel_lsp_references, opts)
-      vim.keymap.set("n", "<leader>fs", tel_grep_string, opts)
+      vim.keymap.set("n", "<leader>ft", tel_grep_string, opts)
+      vim.keymap.set("n", "<leader>fs", tel_lsp_document_symbols, opts)
     end,
   },
   {
