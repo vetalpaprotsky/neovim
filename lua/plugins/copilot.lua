d937869b3d8f6da46311b48b0f5b36d342d3bf6a
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2024-10-06 11:26:51 +0300

    adds copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,1 +1,15 @@
-return { "github/copilot.vim" }
+return {
+  { "github/copilot.vim" },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    build = "make tiktoken",
+    opts = {
+      debug = false,
+    },
+  },
+}
